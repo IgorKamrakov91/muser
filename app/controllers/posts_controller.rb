e9f1ab23427400ef0f6e76183fb517b9922fe9ea
igor <i.kamrakov@yandex.ru>
2016-08-07 12:52:59 +0300

    users added throw devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,21 @@
 class PostsController < ApplicationController
 
   before_action :find_post, only: [:show, :destroy, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.all.order("created_at DESC")
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       redirect_to @post
