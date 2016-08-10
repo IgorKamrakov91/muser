46ae12935aa71d73c9d24861d5dad677b0daded4
igor <i.kamrakov@yandex.ru>
2016-08-10 20:53:12 +0300

    fixing issue#6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,9 +87,9 @@
   #Sets paperclip to upload to amazon s3 by default
   config.paperclip_defaults = {
     storage: :s3,
-    Paperclip::Attachment.default_options[:url] = ':s3_domain_url',
-    Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename',
-    Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com',
+    :url = ':s3_domain_url',
+    :path = '/:class/:attachment/:id_partition/:style/:filename',
+    :s3_host_name = 's3-us-west-2.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
